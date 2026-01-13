4e9b2f11af8673e79309615e8e39d46e18449ba6
mixxue <mixxue@github.com>
2026-01-13 21:40:57 +0700

    feat: Integrate Bootstrap 5 with custom CSS theme

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Bootstrap 5
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,10 +11,15 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Bootstrap 5 CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hc6NmwALlZQSviO+Cst3A5J+eBmUTafZSUf0+DbM5VOUMCU7Gyn" crossorigin="anonymous">
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
+        <!-- Bootstrap 5 JS Bundle -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP8IL0pIyS9T0dNTvBR1v2KqKqnPHVpIkHG" crossorigin="anonymous"></script>
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
